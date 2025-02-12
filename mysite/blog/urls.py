8ced4fbb7d398ad210ce5593ddbe626a95d93b41
cjming354 <jeamingg@gmail.com>
2025-02-12 15:33:00 +0900

    Update urls.py

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from blog.views import home_view, main_view, signup_view, koreanfood, korean_food_list, signup, login
 
 urlpatterns = [
-    path('',index_view ,name='index'),
+    path('',home_view ,name='home'),
     path('main/',main_view, name='main'),
     path('signup_re/',signup, name='signup_re'),
     path('signup/',signup_view, name='signup'),
