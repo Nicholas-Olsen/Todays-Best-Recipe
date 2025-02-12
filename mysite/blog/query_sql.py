4ae69443165df55721653e2a2f50d1ad3b1d98c0
cjming354 <jeamingg@gmail.com>
2025-02-12 16:31:08 +0900

    Update query_sql.py

--- mysite/blog/query_sql.py
+++ mysite/blog/query_sql.py
@@ -29,12 +29,37 @@ def select_password_from_users():
     """
     return sql
 
+
+
+
+
 #recipes 테이블 관련
-def select_food_by_type(): # 음식의 카테고리별로 음식 조회
+def select_foodname_by_type(): # 음식의 카테고리별로 음식 이름 조회
     sql=""" 
     SELECT rec_name from recipes where rec_type = %s
     """
     return sql
+
+def select_descrip_by_type(): # 음식의 카테고리별로 음식 설명 조회 
+    sql=""" 
+    SELECT rec_descrip from recipes where rec_type = %s
+    """
+    return sql
+
+def select_detail_by_type(): # 음식의 카테고리별로 음식 레시피(조리법) 조회회
+    sql=""" 
+    SELECT rec_detail from recipes where rec_type = %s
+    """
+    return sql
+
+def select_ing_by_type(): # 음식의 카테고리별로 음식 사진 조회
+    sql=""" 
+    SELECT rec_img from recipes where rec_type = %s
+    """
+    return sql
+
+
+
 
 #user_ingredients 테이블 관련
 def delete_all_u_i():
@@ -43,30 +68,30 @@ def delete_all_u_i():
     """
     return sql
 
-def insert_ingredient_for_user(): # user_ingredients 테이블에 재료 입력하는 쿼리 (존재하지 않는 재료도 삽입)
-    sql = """
-    INSERT INTO user_ingredients (user_id, ing_id) 
-    VALUES (%s, %s)
-    """
-    return sql
+# def insert_ingredient_for_user(): # user_ingredients 테이블에 재료 입력하는 쿼리 (존재하지 않는 재료도 삽입)
+#     sql = """
+#     INSERT INTO user_ingredients (user_id, ing_id) 
+#     VALUES (%s, %s)
+#     """
+#     return sql
 
-def select_recipes_by_ingredients(): # recipe_ingredients 테이블에서 특정 재료들을 사용하는 레시피 찾기 (일치하는 재료가 n개 이상인 레시피)
-    sql = """
-    SELECT DISTINCT r.rec_name
-    FROM recipes r
-    JOIN recipe_ingredients ri ON r.rec_id = ri.rec_id
-    WHERE ri.ing_id IN (%s)  -- IN 절을 사용해 여러 재료를 처리
-    GROUP BY r.rec_name
-    HAVING COUNT(DISTINCT ri.ing_id) >= %s  -- 사용자가 가진 재료 수와 겹치는 재료가 n개 이상인 레시피
-    """
-    return sql
+# def select_recipes_by_ingredients(): # recipe_ingredients 테이블에서 특정 재료들을 사용하는 레시피 찾기 (일치하는 재료가 n개 이상인 레시피)
+#     sql = """
+#     SELECT DISTINCT r.rec_name
+#     FROM recipes r
+#     JOIN recipe_ingredients ri ON r.rec_id = ri.rec_id
+#     WHERE ri.ing_id IN (%s)  -- IN 절을 사용해 여러 재료를 처리
+#     GROUP BY r.rec_name
+#     HAVING COUNT(DISTINCT ri.ing_id) >= %s  -- 사용자가 가진 재료 수와 겹치는 재료가 n개 이상인 레시피
+#     """
+#     return sql
 
-def insert_new_ingredient(): # ingredients 테이블에 없는 재료를 새로 삽입하는 쿼리
-    sql = """
-    INSERT INTO ingredients (ing_name) 
-    VALUES (%s)
-    """
-    return sql
+# def insert_new_ingredient(): # ingredients 테이블에 없는 재료를 새로 삽입하는 쿼리
+#     sql = """
+#     INSERT INTO ingredients (ing_name) 
+#     VALUES (%s)
+#     """
+#     return sql
 
 def select_ingredients_by_user():
     sql = """
